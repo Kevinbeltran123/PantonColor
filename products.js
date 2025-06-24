adb7c354ae333ab54e1626460e28e44082276811
Kevin Beltran <kevin_beltran@MacBook-Pro-de-Kevin.local>
2025-06-24 11:53:50 -0500

    Cambios precios

--- products.js
+++ products.js
@@ -4,23 +4,23 @@ const products = [
         name: "Caneca - Vinilo Tipo 1",
         description: "Pintura vinílica de máxima duración y recubrimiento, para exteriores e interiores.",
         category: "interior",
-        price: "$85.000",
-        image: "images/MockupVintTipo1.png"
+        price: "$240.000",
+        image: "images/MockupVinTipo1.png"
     },
     {
         id: 2,
-        name: "Caneca - Vinilo Tipo 2",
+        name: "Caneca - Vinilo Tipo 2 Lavable",
         description: "Pintura vinílica, ideal para interiores, de fácil lavado y mantenimiento.",
         category: "interior",
-        price: "$92.000",
+        price: "$180.000",
         image: "images/MockupVinTipo2.png"
     },
     {
         id: 3,
-        name: "Canceca - Vinilo Tipo 3",
+        name: "Canceca - Vinilo Tipo 2 Corriente",
         description: "Pintura para interior, de fácil limpieza.",
         category: "interior",
-        price: "$110.000",
+        price: "$140.000",
         image: "images/MockupVinTipo3.png"
     },
     {
@@ -28,7 +28,7 @@ const products = [
         name: "Bolsa 30 kls - Graniplast Blanco",
         description: "Revestimiento texturizado de alta resistencia ideal para fachadas y muros interiores o exteriores",
         category: "exterior",
-        price: "$150.000",
+        price: "$54.000",
         image: "images/Graniplast.png"
     },
     {
@@ -36,7 +36,7 @@ const products = [
         name: "Caneca - Estuco",
         description: "Es una pasta lista para usar, ideal para alisar y nivelar superficies antes de pintar. Permite obtener paredes lisas, libres de imperfecciones y con un acabado profesional.",
         category: "special",
-        price: "$88.000",
+        price: "$55.000",
         image: "images/MockupEstuco.png"
     },
     {
@@ -44,7 +44,7 @@ const products = [
         name: "Caneca - Mastik",
         description: "Es una masilla versátil y de alta calidad, perfecta para reparar, sellar y rellenar grietas, huecos o juntas en muros, techos y todo tipo de superficies",
         category: "special",
-        price: "$125.000",
-        image: "images/MockupMastik.png"
+        price: "$48.000",
+        image: "images/Mastik.png"
     }
 ];
